2437825547b14e3a488c238ac9f60ef0d87c69a3
meirelon <nestelm@gmail.com>
2019-04-20 13:34:49 -0400

    new versions of pandas gbq

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
 from deps.statcast import get_statcast_data
 import pandas as pd
+import pandas_gbq
 
 def statcast_request(request):
     project = os.environ["PROJECT_ID"]
     destination_table = os.environ["DESTINATION_TABLE"]
     # key = os.environ["KEY"]
     df = get_statcast_data()
-    df.to_gbq(project_id=project,
-              destination_table=destination_table,
-              if_exists='append')
+    pandas_gbq.to_gbq(df, project_id=project,
+                      destination_table=destination_table,
+                      if_exists="append")
