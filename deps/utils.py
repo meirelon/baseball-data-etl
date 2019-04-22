da7197f5ea11bb2a89fea3068386495a8a57b7cb
meirelon <nestelm@gmail.com>
2019-04-22 17:01:30 -0400

    tab block

--- deps/utils.py
+++ deps/utils.py
@@ -8,12 +8,12 @@ def get_date_range(start="2019-03-15", end=datetime.now().strftime("%Y-%m-%d")):
 def get_gamelog_range(date_range):
     game_logs = []
     for d in date_range:
-    try:
-      b = batting_stats_range(d)
-      b['date'] = d
-      game_logs.append(b)
-    except:
-      next
+        try:
+          b = batting_stats_range(d)
+          b['date'] = d
+          game_logs.append(b)
+        except:
+          next
     game_logs_df = pd.concat(game_logs, axis=0, ignore_index=True)
     game_logs_df.columns = [c.replace("#", "").replace("2", "_2").replace("3", "_3") for c in game_logs_df.columns]
     return game_logs_df
