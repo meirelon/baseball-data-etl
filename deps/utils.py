2d24dbc20ccc53762085cce5582edaf6cd9223fd
meirelon <nestelm@gmail.com>
2019-04-22 16:58:53 -0400

    changing tab delim

--- deps/utils.py
+++ deps/utils.py
@@ -6,14 +6,14 @@ def get_date_range(start="2019-03-15", end=datetime.now().strftime("%Y-%m-%d")):
   return [x.strftime("%Y-%m-%d") for x in pd.date_range(start=start, end=end)]
 
 def get_gamelog_range(date_range):
-  game_logs = []
-  for d in date_range:
+    game_logs = []
+    for d in date_range:
     try:
       b = batting_stats_range(d)
       b['date'] = d
       game_logs.append(b)
     except:
       next
-  game_logs_df = pd.concat(game_logs, axis=0, ignore_index=True)
-  game_logs_df.columns = [c.replace("#", "").replace("2", "_2").replace("3", "_3") for c in game_logs_df.columns]
-  return game_logs_df
+    game_logs_df = pd.concat(game_logs, axis=0, ignore_index=True)
+    game_logs_df.columns = [c.replace("#", "").replace("2", "_2").replace("3", "_3") for c in game_logs_df.columns]
+    return game_logs_df
