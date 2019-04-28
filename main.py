8f45226aa309fc783a09959cfa9e4a51ae7d808f
meirelon <nestelm@gmail.com>
2019-04-28 15:52:17 -0400

    adding formating

--- main.py
+++ main.py
@@ -7,31 +7,31 @@ from deps.utils import get_gamelog_range, probablePitchers, mlb_injuries, get_da
 from deps.seatgeek import seatgeek
 
 def mlb_daily_etl(request):
-    #DEFINE ENVIRONMENT VARIABLES
+    # DEFINE ENVIRONMENT VARIABLES
     project = os.environ["PROJECT_ID"]
     dataset = os.environ["DATASET"]
     today = datetime.now().strftime("%Y-%m-%d")
     yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
 
-    #DAILY STATCAST DATA FROM BASEBALL SAVANT
+    # DAILY STATCAST DATA FROM BASEBALL SAVANT
     df = get_statcast_data()
     pandas_gbq.to_gbq(df, project_id=project,
                       destination_table="{dataset}.statcast_{dt}".format(dataset=dataset, dt=yesterday.replace("-","")),
                       if_exists="replace")
 
-    #DAILY STANDARD GAME LOGS
+    # DAILY STANDARD GAME LOGS
     df = get_gamelog_range([yesterday])
     pandas_gbq.to_gbq(df, project_id=project,
                       destination_table="{dataset}.batting_{dt}".format(dataset=dataset, dt=yesterday.replace("-","")),
                       if_exists="replace")
 
-    #DAILY INJURY REPORT FROM MLBAM
+    # DAILY INJURY REPORT FROM MLBAM
     df = mlb_injuries()
     pandas_gbq.to_gbq(df, project_id=project,
               destination_table="{dataset}.injuries_{dt}".format(dataset=dataset, dt=today.replace("-","")),
               if_exists="replace")
 
-    #PROBABLE PITCHERS OVER NEXT THREE DAYS FROM MLBAM
+    # PROBABLE PITCHERS OVER NEXT THREE DAYS FROM MLBAM
     date_range = get_date_range_days(start=0, end=2)
     for dt in date_range:
       dt_split = [int(x) for x in dt.split("-")]
@@ -43,16 +43,16 @@ def mlb_daily_etl(request):
 
 
 def seatgeek_events(request):
-    #DEFINE ENVIRONMENT VARIABLES
+    # DEFINE ENVIRONMENT VARIABLES
     project = os.environ["PROJECT_ID"]
     dataset = os.environ["DATASET"]
     seat_geek_client_id = os.environ["SEAK_GEEK_CLIENT_ID"]
     seat_geek_secret = os.environ["SEAT_GEEK_SECRET"]
     today = datetime.now().strftime("%Y-%m-%d")
 
-    #DAILY INJURY REPORT FROM MLBAM
+    # DAILY INJURY REPORT FROM MLBAM
     geek = seatgeek(client=seat_geek_client_id, secret=seat_geek_secret)
-    df =geek_run()
+    df =geek.run()
     pandas_gbq.to_gbq(df, project_id=project,
               destination_table="{dataset}.tickets_{dt}".format(dataset=dataset, dt=today.replace("-","")),
               if_exists="replace")
