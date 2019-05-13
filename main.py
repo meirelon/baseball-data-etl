ae86c193da1cb9ef1c408752fa321239d472c6a4
meirelon <nestelm@gmail.com>
2019-05-13 14:05:57 -0400

    fixing indent

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def mlb_daily_etl(request):
                       destination_table="{dataset}.batting_{dt}".format(dataset=dataset, dt=yesterday.replace("-","")),
                       if_exists="replace")
 
+
      # DAILY STANDARD GAME LOGS
      df = utils.get_gamelog_range(date_range=[yesterday], game_log_type="pitching")
      pandas_gbq.to_gbq(df, project_id=project,
